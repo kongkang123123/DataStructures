9ced8b1bfcd391d4bee8d19b3bf035b92d4230ee
KimMinseong <rjaehno2@naver.com>
2024-07-03 22:52:26 +0900

    feat: Add 'Array List codes

--- Linked Lists/열혈 자료구조 예제/Array List/ListMain.c
+++ Linked Lists/열혈 자료구조 예제/Array List/ListMain.c
@@ -1,45 +1,52 @@
+// 윤성우의 열혈 자료구조 p.82
 #include <stdio.h>
 #include "ArrayList.h"
 
 int main(void) {
-    // ArrayList의 생성 및 초기화
+    // 문제 03-1. 1번: ArrayList의 생성 및 초기화
     List list;
     int data;
     ListInit(&list);
 
-    // 5개의 데이터 저장
-    LInsert(&list, 11);    LInsert(&list, 11);
-    LInsert(&list, 22);    LInsert(&list, 22);
-    LInsert(&list, 33);
+    LInsert(&list, 1);
+    LInsert(&list, 2);
+    LInsert(&list, 3);
+    LInsert(&list, 4);
+    LInsert(&list, 5);
+    LInsert(&list, 6);
+    LInsert(&list, 7);
+    LInsert(&list, 8);
+    LInsert(&list, 9);
 
-    // 저장된 데이터의 전체 출력
-    printf("The number of data: %d \n", LCount(&list));
-
-    if(LFirst(&list, &data)) {    // 첫 번째 데이터 조회
-        printf("%d ", data);
-
-        while(LNext(&list, &data)) {
-            printf("%d ", data);
+    // 문제 03-01. 2번: 저장된 값의 합 계산하여 출력
+    int sum = 0;
+    if(LFirst(&list, &data))
+    {
+        sum += data;
+        while(LNext(&list, &data))
+        {
+            sum += data;
         }
     }
-    printf("\n\n");
+    printf("Total: %d\n", sum);
 
-    // 숫자 22을 탐색하여 모두 삭제
-    if(LFirst(&list, &data)) {
-        if(data == 22) {
+    // 문제 03-01. 3번: 2또는 3의 배수의 데이터 삭제
+    if(LFirst(&list, &data)) 
+    {
+       if(data % 2 == 0 || data % 3 == 0) 
+       {
             LRemove(&list);
-        }
+       }
 
-        while(LNext(&list, &data)) {
-            if(data == 22) {
+       while(LNext(&list, &data)) {
+            if(data % 2 == 0 || data % 3 == 0) 
+            {
                 LRemove(&list);
             }
         }
     }
 
-    // 삭제 후 남은 데이터 전체 출력
-    printf("The number of data: %d\n", LCount(&list));
-
+    // 문제 03-01. 4번: 리스트에 저장된 데이터 순서대로 출력
     if(LFirst(&list, &data)) {
         printf("%d ", data);
 
