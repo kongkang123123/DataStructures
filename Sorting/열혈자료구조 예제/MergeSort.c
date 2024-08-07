38e4cf537568ca35ae5b683829ec038859bc28fe
KimMinseong <rjaehno2@naver.com>
2024-08-08 01:01:09 +0900

    Add comment to 'Merge Sort' code

--- Sorting/열혈자료구조 예제/MergeSort.c
+++ Sorting/열혈자료구조 예제/MergeSort.c
@@ -8,11 +8,14 @@ void MergeTwoArea(int arr[], int left, int mid, int right)
     int rIdx = mid+1;
     int i;
 
+    // 병합 한 결과를 담을 배열 sortArr의 동적 할당
     int * sortArr = (int*)malloc(sizeof(int)*(right+1));
     int sIdx = left;
 
     while(fIdx <= mid && rIdx <= right)
     {
+        // 병합 할 두 영역의 데이터들을 비교하여,
+        // 정렬순서대로 sortArr에 하나씩 옮겨 담는다.
         if(arr[fIdx] <= arr[rIdx])
         {
             sortArr[sIdx] = arr[fIdx++];
@@ -25,16 +28,17 @@ void MergeTwoArea(int arr[], int left, int mid, int right)
         sIdx++;
     }
 
-    if(mid < fIdx)
+    // 이후엔 비교가 무의미해짐. 반으로 나눴을때, part1과 part2 모두 정렬이 되어있는 상태이기 때문
+    if(mid < fIdx)  // 배열의 앞부분이 모두 sortArr에 옮겨졌다면,
     {
-        for(i=rIdx; i<=right; i++, sIdx++)
+        for(i=rIdx; i<=right; i++, sIdx++)  // 배열의 뒷부분에 남은 데이터들을 sortArr에 그대로 옮긴다.
         {
             sortArr[sIdx] = arr[i];
         }
     }
-    else
+    else            // 배열의 뒷부분이 모두 sortArr에 옮겨졌다면,
     {
-        for(i=fIdx; i<=mid; i++, sIdx++)
+        for(i=fIdx; i<=mid; i++, sIdx++)    // 배열의 앞부분에 남은 데이터들을 sortArr에 그대로 옮긴다.
         {
             sortArr[sIdx] = arr[i];
         }
